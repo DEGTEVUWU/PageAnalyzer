623955d2db1813a6cddf8df8513674d87ea5cbaa
ivan dyogtev <diogteff.ivan@yandex.ru>
2024-03-22 11:39:09 +0200

    deleted java plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,6 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 plugins {
     application
     checkstyle
-    java
     jacoco
     id("checkstyle")
     id("io.freefair.lombok") version "8.4"
